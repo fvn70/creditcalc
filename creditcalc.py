3e3d71870ca7e97ed355b9b71e771d2aba87d793
fvn70 <fvn_reg@mail.ru>
2021-10-25 21:21:11 +0700

    Ver 3/4

--- creditcalc.py
+++ creditcalc.py
@@ -1,11 +1,73 @@
-loan_principal = 'Loan principal: 1000'
-final_output = 'The loan has been repaid!'
-first_month = 'Month 1: repaid 250'
-second_month = 'Month 2: repaid 250'
-third_month = 'Month 3: repaid 500'
+import math
 
-print(loan_principal)
-print(first_month)
-print(second_month)
-print(third_month)
-print(final_output)
+
+def calc_period(p, a, per):
+    # interest rate
+    i = per / 12 / 100
+    # number of months
+    n = math.log(a / (a - i * p), 1 + i)
+    m = math.floor(n)
+    if n > m:
+        m += 1
+    y = m // 12
+    m -= y * 12
+    word = ""
+    if y > 0:
+        w_year = "years" if y > 1 else "year"
+        word += f"{y} {w_year}{' and ' if m > 0 else ''}"
+    if m > 0:
+        w_month = "months" if m > 1 else "month"
+        word += f"{m} {w_month}"
+    print(f"It will take {word} to repay this loan!")
+
+
+def calc_payment(p, n, per):
+    # interest rate
+    i = per / 12 / 100
+    j = (1 + i) ** n
+    a = math.ceil(p * i * j / (j - 1))
+    last_pay = p - a * n
+    result = "Your monthly payment = " + str(a)
+    if last_pay > 0:
+        result += " and the last payment = " + str(last_pay)
+    print(result + "!")
+
+
+def calc_apayment(a, n, per):
+    # interest rate
+    i = per / 12 / 100
+    j = (1 + i) ** n
+    p = round(a * (j - 1) / (i * j))
+    print(f"Your loan principal = {p}!")
+
+
+print('''What do you want to calculate?
+type "n" for number of monthly payments,
+type "a" for annuity monthly payment amount,
+type "p" for loan principal:''')
+cmd = input()
+
+if cmd == 'n':
+    print("Enter the loan principal:")
+    lp = int(input())
+    print("Enter the monthly payment:")
+    mp = int(input())
+    print("Enter the loan interest:")
+    pr = float(input())
+    calc_period(lp, mp, pr)
+elif cmd == 'a':
+    print("Enter the loan principal:")
+    lp = int(input())
+    print("Enter the number of periods:")
+    m = int(input())
+    print("Enter the loan interest:")
+    pr = float(input())
+    calc_payment(lp, m, pr)
+else:
+    print("Enter the annuity payment:")
+    ap = float(input())
+    print("Enter the number of periods:")
+    m = int(input())
+    print("Enter the loan interest:")
+    pr = float(input())
+    calc_apayment(ap, m, pr)
